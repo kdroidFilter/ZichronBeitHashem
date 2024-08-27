e6c6007eb02122f2d971cfbd80f3d366b2068556
elyahou <elyahou.hadass@gmail.com>
2024-08-27 18:52:42 +0300

    ajustement of the background

--- app/src/main/java/com/kdroid/zichronbeithashem/features/screens/home/Home.kt
+++ app/src/main/java/com/kdroid/zichronbeithashem/features/screens/home/Home.kt
@@ -2,13 +2,20 @@
 
 package com.kdroid.zichronbeithashem.features.screens.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.kdroid.zichronbeithashem.core.presentation.background.BackgroundImage
+import androidx.compose.ui.unit.dp
+import com.kdroid.zichronbeithashem.R
 import com.kdroid.zichronbeithashem.core.presentation.navigation.MainNavigationState
 import com.kdroid.zichronbeithashem.core.presentation.screensize.determineScreenSize
 import com.kdroid.zichronbeithashem.core.presentation.theme.AppTheme
@@ -29,20 +36,30 @@ fun Home(navigationState: MainNavigationState) {
 @Composable
 private fun HomeScreen(homeState: HomeState, onEvent: (HomeEvents) -> Unit) {
     Scaffold { paddingValues ->
-        BoxWithConstraints(modifier = Modifier) {
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
             val screenSize = determineScreenSize(maxHeight, maxWidth)
-            BackgroundImage.Display(
-                modifier = Modifier.matchParentSize(),
-                maxWidth = maxWidth,
-                maxHeight = maxHeight
-            )
-            Ornements(screenSize).Display()
-            Header(
-                paddingValues = paddingValues,
-                screenSize = screenSize,
-                pastedYears = homeState.timeInterval.years
-            )
-            Parchemin(onEvent, homeState)
+            val alignment = if (maxHeight < 700.dp || maxWidth / maxHeight > 0.65f) {
+                Alignment.Center
+            } else {
+                Alignment.TopCenter
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .paint(
+                        painter = painterResource(id = R.drawable.background),
+                        contentScale = ContentScale.FillBounds,
+                        alignment = alignment
+                    )
+            ) {
+                Ornements(screenSize).Display()
+                Header(
+                    paddingValues = paddingValues,
+                    screenSize = screenSize,
+                    pastedYears = homeState.timeInterval.years
+                )
+                Parchemin(onEvent, homeState)
+            }
         }
     }
 }
@@ -52,7 +69,7 @@ private fun HomeScreen(homeState: HomeState, onEvent: (HomeEvents) -> Unit) {
 @Composable
 fun HomeScreenPreview() {
     AppTheme {
-        HomeScreen(homeState = HomeState.previewState, {}, )
+        HomeScreen(homeState = HomeState.previewState, {})
     }
 }
 
